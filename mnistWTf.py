159a8bece72fe1692c85ee57726fcdd398efa82f
Brian Martin <brianlmart@gmail.com>
2023-03-24 14:49:08 -0600

    Basic model structure

--- mnistWTf.py
+++ mnistWTf.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.keras import Model
 import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
@@ -12,3 +14,26 @@ print(len(x_test))
 
 print(y_train[0])
 plt.imshow(x_train[0], cmap='gray')
+
+class MNISTModel(Model):
+    def __init__(self):
+        super(MNISTModel, self).__init__()
+
+        # Conv2D(filters, kernel_size, activation)
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        # Flatten() - 2d array to 1d array
+        self.flatten = Flatten()
+        # Dense(neurons, activation) - neural network layer
+        self.dense1 = Dense(128, activation='relu')
+        self.dense2 = Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.dense1(x)
+        return self.dense2(x)
+
+
+model = MNISTModel()
+model(...)
+
