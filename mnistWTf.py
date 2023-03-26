66c4bc884aea36b62405982eecdec9076b2f2bdc
Brian Martin <brianlmart@gmail.com>
2023-03-25 20:25:00 -0600

    More Additions

--- mnistWTf.py
+++ mnistWTf.py
@@ -13,7 +13,7 @@ print(len(x_train))
 print(len(x_test))
 
 print(y_train[0])
-plt.imshow(x_train[0], cmap='gray')
+plt.imshow(x_train[0], cmap="gray")
 
 class MNISTModel(Model):
     def __init__(self):
@@ -33,7 +33,30 @@ class MNISTModel(Model):
         x = self.dense1(x)
         return self.dense2(x)
 
-
 model = MNISTModel()
-model(...)
 
+loss_function = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()  # modifies learning rate on well it's doing
+
+train_loss = tf.keras.metrics.Mean(name="train_loss")
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name="train_accuracy")
+test_loss = tf.keras.metrics.Mean(name="test_loss")
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name="test_accuracy")
+
+@tf.function
+def train_step(inputs, outputs):
+    with tf.GradientTape() as tape:
+        predictions = model(inputs)
+        loss = loss_function(outputs, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(outputs, predictions)
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
